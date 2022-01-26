1ffaea19e77065fdcfdc13bc05667348cacf4fd2
athetoin <athetoin@gmail.com>
2022-01-26 01:08:01 -0500

    Added 1.5

---
+++ cloud/static-site/gt-cs1331/public/scripts/1-05-computator.js
@@ -0,0 +1,9 @@
+class Solution105 extends Solution {
+  id = "1.5";
+  title = "Computator";
+  answer() {
+    return [((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)).toFixed(6)];
+  }
+}
+
+allSolutions.push(new Solution105());
--- cloud/static-site/gt-cs1331/public/scripts/dispatcher.js
+++ cloud/static-site/gt-cs1331/public/scripts/dispatcher.js
@@ -1,74 +1,61 @@
 var scriptsForLoad = 0;
 document.getElementById("run").onclick = executeSolution;
-var selector = document.getElementById("solutions")
+var selector = document.getElementById("solutions");
 
-class Solution{
-    id; title;
-    isPattern = false;
-    answer(){return any;}
-    get orderNumber(){
-        var parts = this.id.split(".");
-        return parseInt(parts[0],10) * 100 + parseInt(parts[1],10);
-    }
-    get label(){
-        return this.id + ": " + this.title
-    }
-}
-
-var allSolutions = [];
-
-window.onload = function() {
-    var solutionNames = [
+window.onload = function () {
+  var solutionNames = [
+    "solution",
     "1-01-three-messages",
     "1-02-five-messages",
     "1-03-pattern",
-    "1-04-table"
-    ];
-    scriptsForLoad = solutionNames.length;
-    solutionNames.forEach(name => {
-        var newScript = document.createElement("script");
-        newScript.src = "scripts/" + name + ".js";
-        newScript.async = true;
-        newScript.onload = onLoadedScript;
-        document.head.appendChild(newScript);
-    })
-}
+    "1-04-table",
+    "1-05-computator",
+  ];
+  scriptsForLoad = solutionNames.length;
+  solutionNames.forEach((name) => {
+    var newScript = document.createElement("script");
+    newScript.src = "scripts/" + name + ".js";
+    newScript.async = true;
+    newScript.onload = onLoadedScript;
+    document.head.appendChild(newScript);
+  });
+};
 
 function onLoadedScript() {
-    scriptsForLoad--;
-    if(scriptsForLoad == 0){
-        allSolutions.sort((a,b)=>a.orderNumber - b.orderNumber);
-        allSolutions.forEach(solution => {
-            var newOption = document.createElement("option");
-            newOption.value = solution.id;
-            newOption.innerText = solution.label;
-            selector.append(newOption);
-        })
-    }
+  scriptsForLoad--;
+  if (scriptsForLoad == 0) {
+    allSolutions.sort((a, b) => a.orderNumber - b.orderNumber);
+    allSolutions.forEach((solution) => {
+      var newOption = document.createElement("option");
+      newOption.value = solution.id;
+      newOption.innerText = solution.label;
+      selector.append(newOption);
+    });
+  }
 }
 
 function executeSolution() {
-    if(selector.value != ""){
-        var out = document.getElementById("out");
-        out.querySelectorAll("p").forEach(p => out.removeChild(p));
+  if (selector.value != "") {
+    var out = document.getElementById("out");
+    out.querySelectorAll("p").forEach((p) => out.removeChild(p));
 
-        var solution = allSolutions.filter(item => item.id == selector.value)[0];
-        var title = document.getElementById("title");
-        title.innerText = solution.label;
+    var solution = allSolutions.filter((item) => item.id == selector.value)[0];
+    var title = document.getElementById("title");
+    title.innerText = solution.label;
 
-        var answer = solution.answer();
-        if(!solution.isPattern){
-            answer.forEach(output=> {
-                var paragraph = document.createElement('p');
-                out.append(paragraph);
-                paragraph.innerText = output;
-            });
-        } else {
-            var paragraph = document.createElement('p');
-            out.append(paragraph);
-            var preformated = document.createElement('pre');
-            paragraph.append(preformated);
-            preformated.innerHTML = answer.join('<br>');
-        }
+    var answer = solution.answer();
+    if (!solution.isPattern) {
+      answer.forEach((output) => {
+        var paragraph = document.createElement("p");
+        out.append(paragraph);
+        paragraph.innerText = output;
+      });
+    } else {
+      var paragraph = document.createElement("p");
+      out.append(paragraph);
+      var preformated = document.createElement("pre");
+      paragraph.append(preformated);
+      preformated.innerHTML = answer.join("<br>");
     }
+  }
 }
