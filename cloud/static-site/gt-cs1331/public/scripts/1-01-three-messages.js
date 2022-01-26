4a05a246440e22962130a084a4ced3795c030f4a
athetoin <athetoin@gmail.com>
2022-01-26 01:09:07 -0500

    Reformated JS

--- cloud/static-site/gt-cs1331/public/scripts/1-01-three-messages.js
+++ cloud/static-site/gt-cs1331/public/scripts/1-01-three-messages.js
@@ -1,13 +1,13 @@
 class Solution101 extends Solution {
-    id = "1.1"
-    title = "Display three messages"
-    answer() {
-        return [
-            'Welcome to Java',
-            'Welcome to Computer Science',
-            'Programming is fun'
-            ];
-    }
+  id = "1.1";
+  title = "Display three messages";
+  answer() {
+    return [
+      "Welcome to Java",
+      "Welcome to Computer Science",
+      "Programming is fun",
+    ];
+  }
 }
 
 allSolutions.push(new Solution101());
--- cloud/static-site/gt-cs1331/public/scripts/1-02-five-messages.js
+++ cloud/static-site/gt-cs1331/public/scripts/1-02-five-messages.js
@@ -1,9 +1,9 @@
 class Solution102 extends Solution {
-    id = "1.2"
-    title = "Display five messages"
-    answer() {
-        return Array.from(Array(5),()=>"Welcome to Programming");
-    }
+  id = "1.2";
+  title = "Display five messages";
+  answer() {
+    return Array(5).fill("Welcome to Java");
+  }
 }
 
 allSolutions.push(new Solution102());
