c48a370f003facb426efc55d3ad7d45d03123204
athetoin <athetoin@gmail.com>
2022-01-27 22:46:43 -0500

    Formated 2.12

--- cloud/static-site/gt-cs1331/test/scripts/1-12-speed-calc.spec.js
+++ cloud/static-site/gt-cs1331/test/scripts/1-12-speed-calc.spec.js
@@ -2,7 +2,6 @@ describe("Solution 1.12", function () {
   describe("#answer()", function () {
     it("should return 8.947804", function () {
       var solution = new Solution112();
-      var result = solution.answer();
       expect("8.947804").is.equal(solution.answer()[0].split(" ")[0]);
     });
   });
