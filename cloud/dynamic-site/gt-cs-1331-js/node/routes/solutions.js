64eaeea5fb93749580f431d085f277a5fc610251
athetoin <athetoin@gmail.com>
2022-02-09 22:03:59 -0500

    fixed eval problem

--- cloud/dynamic-site/gt-cs-1331-js/node/routes/solutions.js
+++ cloud/dynamic-site/gt-cs-1331-js/node/routes/solutions.js
@@ -2,19 +2,20 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const vm = require("vm");
-const console =require("console")
-console.log("Hi")
-console.log(__dirname)
+const console = require("console");
+console.log("Hi");
+console.log(__dirname);
 const allSolutions = fs
   .readdirSync(__dirname + "/../solutions/")
   .map((name) => name.split(".")[0])
   .filter((name) => name !== "solution")
-  .map((name) => eval(`new (require("../solutions/${name}"))`) /*{
-    const code = fs.readFileSync(__dirname + "/../solutions/" + name + ".js", "utf-8");
-    const sandbox={require,console,__dirname}
-    const obj = vm.runInNewContext(code, sandbox);
-    return new obj();
-  }*/);
+  .map((name) =>
+    vm.runInNewContext(`new (require("../solutions/${name}"))`, {
+      require,
+      __filename,
+      __dirname,
+    })
+  );
 
 router.get("/", function (req, res, next) {
   res.send(
